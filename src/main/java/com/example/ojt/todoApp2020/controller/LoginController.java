c99ac471f3442e9b50bc06e3265657afc61ccdfb
matsu4ki <matsu4ki07@gmail.com>
2020-06-05 05:12:30 +0900

    [add]FaceBookとGoogleのOAuthログインが表示されるようにした。

--- src/main/java/com/example/ojt/todoApp2020/controller/LoginController.java
+++ src/main/java/com/example/ojt/todoApp2020/controller/LoginController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 @Controller
 public class LoginController {
 
-    private static String authorizationRequestBaseUri
+    private static final String authorizationRequestBaseUri
         = "oauth2/authorization";
     Map<String, String> oauth2AuthenticationUrls
         = new HashMap<>();
@@ -25,18 +25,22 @@ public class LoginController {
      */
     @GetMapping("/login")
     public String getLoginPage(Model model) {
-//        Iterable<ClientRegistration> clientRegistrations = null;
+        Iterable<ClientRegistration> clientRegistrations = null;
 //        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository)
 //            .as(Iterable.class);
 //        if (type != ResolvableType.NONE &&
 //            ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
 //            clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
 //        }
-//
+
 //        clientRegistrations.forEach(registration ->
 //            oauth2AuthenticationUrls.put(registration.getClientName(),
 //                authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
-//        model.addAttribute("urls", oauth2AuthenticationUrls);
+        oauth2AuthenticationUrls.put("facebook",
+            authorizationRequestBaseUri + "/" + "facebook");
+        oauth2AuthenticationUrls.put("google",
+            authorizationRequestBaseUri + "/" + "google");
+        model.addAttribute("urls", oauth2AuthenticationUrls);
 
         return "login";
     }
