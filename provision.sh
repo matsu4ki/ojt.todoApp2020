5191371d8936daf2ae5509d7c3ac14f1df84fb0a
matsu4ki <matsu4ki07@gmail.com>
2020-06-12 09:31:09 +0900

    [add]nginxおよびtomcatのインストールを追加した
    #1

--- provision.sh
+++ provision.sh
@@ -2,37 +2,61 @@
 # OverView
 # This script is CentOS8 initializer used by Vagrant.
 
+# Set Function
+function SectionLine(){
+   i=100
+   printf "%${i}s\n" | tr ' ' '-'
+   echo "$1"
+   printf "%${i}s\n" | tr ' ' '-'
+}
+
+
+############################################################################################
+# locale setting
+############################################################################################
+SectionLine "locale setting"
+sudo dnf install -y glibc-langpack-ja langpacks-ja
+localectl set-locale LANG=ja_JP.utf8
+localectl status
+
+
+############################################################################################
 # upgrade packages
 # dnf updateはdnf upgradeのエイリアス
 # 効果に違いはない（dnf updateは将来廃止されるかもしれない）
 # https://qiita.com/yunano/items/eedd3da40d67394881a2
-#####################
-# 時間がかかるのでdisabled
-# sudo dnf upgrade -y
-#####################
+############################################################################################
+SectionLine "upgrade packages"
+sudo dnf clean all
+sudo rm -r /var/cache/dnf
+sudo dnf upgrade -y
 
-# locale setting
-echo '================== locale setting start =================='
-sudo dnf install -y glibc-langpack-ja
-localectl set-locale LANG=ja_JP.utf8
-localectl status
-echo '================== locale setting end =================='
 
-# 時刻の同期を行う
-echo '================== sync time start =================='
+############################################################################################
+# sync time
+############################################################################################
+SectionLine "sync time"
 timedatectl set-timezone Asia/Tokyo
 date && chronyc -a makestep && date
-echo '================== sync time end =================='
 
+
+############################################################################################
 # install expect
+############################################################################################
+SectionLine "install expect"
 sudo dnf install -y expect
 
-# DB settings
-echo '================== mysql setting start =================='
+
+############################################################################################
+# mysql settings
+############################################################################################
+SectionLine "mysql install"
 sudo dnf remove -y mariadb
 sudo dnf install -y @mysql:8.0
 sudo systemctl enable mysqld
 sudo systemctl start mysqld
+
+SectionLine "mysql setting"
 expect -c "
 spawn mysql_secure_installation
 expect \"Press y|Y for Yes, any other key for No: y\"
@@ -77,4 +101,115 @@ expect \"Enter password:\"
 send  \"rootuser\n\"
 expect "
 
-echo '================== mysql setting end =================='
+
+############################################################################################
+# Install Vim
+############################################################################################
+SectionLine "install vim"
+sudo dnf -y install vim
+
+############################################################################################
+# Install tar
+############################################################################################
+SectionLine "install tar"
+sudo dnf -y install tar
+
+############################################################################################
+# Setup nginx
+############################################################################################
+SectionLine "install nginx"
+sudo dnf install -y nginx
+
+SectionLine "Backup Conf"
+sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.org
+sudo ls -ltar /etc/nginx/nginx.conf*
+
+SectionLine "Set /etc/nginx/nginx.conf"
+sudo cp -f /vagrant/conf/nginx.conf /etc/nginx/nginx.conf
+sudo cat /etc/nginx/nginx.conf
+
+SectionLine "Set /etc/nginx/conf.d/tomcat.conf"
+sudo cp -f /vagrant/conf/tomcat.conf /etc/nginx/conf.d/tomcat.conf
+sudo cat /etc/nginx/conf.d/tomcat.conf
+
+SectionLine "Enable nginx"
+sudo systemctl enable nginx.service
+
+SectionLine "Start nginx"
+sudo systemctl start nginx.service
+sudo systemctl status nginx.service
+
+############################################################################################
+# Setup openjdk
+############################################################################################
+SectionLine "Create /etc/yum.repos.d/adoptopenjdk.repo"
+sudo echo '[AdoptOpenJDK]
+name=AdoptOpenJDK
+baseurl=http://adoptopenjdk.jfrog.io/adoptopenjdk/rpm/centos/8/x86_64
+enabled=1
+gpgcheck=1
+gpgkey=https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public' > /etc/yum.repos.d/adoptopenjdk.repo
+sudo cat /etc/yum.repos.d/adoptopenjdk.repo
+
+SectionLine "install adoptopenjdk"
+sudo dnf -y install adoptopenjdk-11-hotspot.x86_64
+
+SectionLine "java version"
+java -version
+
+############################################################################################
+# Setup tomcat
+############################################################################################
+SectionLine "Useradd tomcat"
+sudo useradd -s /sbin/nologin tomcat
+cat /etc/passwd | grep tomcat
+
+SectionLine "install tomcat9"
+cd /tmp
+sudo curl -O https://ftp.tsukuba.wide.ad.jp/software/apache/tomcat/tomcat-9/v9.0.36/bin/apache-tomcat-9.0.36.tar.gz
+sudo tar -zxvf apache-tomcat-9.0.36.tar.gz -C /opt
+
+sudo ln -s /opt/apache-tomcat-9.0.36 /opt/apache-tomcat
+sudo chown -R tomcat. /opt/apache-tomcat-9.0.36
+sudo ls -ltar /opt/apache-tomcat
+sudo ls -ltar /opt/apache-tomcat-9.0.36
+
+SectionLine "Set /etc/profile.d/tomcat.sh"
+sudo echo "export CATALINA_HOME=/opt/apache-tomcat" > /etc/profile.d/tomcat.sh
+sudo cat /etc/profile.d/tomcat.sh
+
+SectionLine "Create /etc/tomcat"
+sudo ln -s /opt/apache-tomcat/conf /etc/tomcat
+sudo ls -ltar /etc/tomcat
+
+SectionLine "Set Environment"
+echo "export SPRING_PROFILES_ACTIVE=prod" > /opt/apache-tomcat/bin/setup.sh
+
+SectionLine "Create tomcat.service"
+echo '[Unit]
+Description=Apache Tomcat 9
+After=syslog.target network.target
+
+[Service]
+User=tomcat
+Group=tomcat
+Type=oneshot
+PIDFile=/opt/apache-tomcat/tomcat.pid
+RemainAfterExit=yes
+
+ExecStart=/opt/apache-tomcat/bin/startup.sh
+ExecStop=/opt/apache-tomcat/bin/shutdown.sh
+ExecReStart=/opt/apache-tomcat/bin/shutdown.sh;/opt/apache-tomcat/bin/startup.sh
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/tomcat.service
+sudo cat /etc/systemd/system/tomcat.service
+
+SectionLine "Enable tomcat.service"
+sudo chmod 755 /etc/systemd/system/tomcat.service
+sudo ls -ltar /etc/systemd/system/tomcat.service
+sudo systemctl enable tomcat.service
+
+SectionLine "Start tomcat.service"
+sudo systemctl start tomcat.service
+sudo systemctl status tomcat.service
